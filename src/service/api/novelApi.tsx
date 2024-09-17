5d5c8f6cbd9cc5c70ef045a4d7d3eae37975ce2f
MinhTranQuoc <minhtranquoc2025@gmail.com>
2024-09-18 01:39:23 +0700

    finish login by username or email

--- src/service/api/novelApi.tsx
+++ src/service/api/novelApi.tsx
@@ -1,19 +1,38 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery, FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
 import { NovelResponseDto } from "../../interfaces/NovelResponseDto";
 import { Page } from "../../interfaces/Page";
+import { BaseQueryFn, FetchArgs } from "@reduxjs/toolkit/query";
+import { logout } from "../../components/User/authSlice";
 
-export const novelApi = createApi({
-  reducerPath: "novelApi",
-  baseQuery: fetchBaseQuery({
+
+// Tạo baseQuery với xử lý lỗi và logout
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+  const baseQuery = fetchBaseQuery({
     baseUrl: "http://localhost:8080/novel",
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem('token'); // Lấy token từ localStorage
+      const token = localStorage.getItem('token'); // Lấy token từ state
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`); // Thêm token vào tiêu đề Authorization
+        headers.set('Authorization', `Bearer ${token}`);
       }
       return headers;
     },
-  }),
+  });
+
+  const result = await baseQuery(args, api, extraOptions);
+
+  // Kiểm tra nếu có lỗi và là lỗi 401
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());  // Thực hiện hành động logout
+
+  }
+
+  return result;
+};
+
+// Tạo API với baseQuery mới
+export const novelApi = createApi({
+  reducerPath: "novelApi",
+  baseQuery: baseQueryWithReauth,
   endpoints: (builder) => ({
     getAllNovels: builder.query<
       Page<NovelResponseDto>,
